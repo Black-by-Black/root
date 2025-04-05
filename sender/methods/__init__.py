03178f038f44f40a39c0dddb03f53924fa9e7e1e
Black-by-Black <red201120112011@gmail.com>
2025-04-05 18:31:28 +0300

    Update __init__.py

--- sender/methods/__init__.py
+++ sender/methods/__init__.py
@@ -1,5 +1,6 @@
 from .tcp import tcp
 from .udp import udp
+from sender.utils.logger import PacketLogger
 
 _methods = {
     'tcp': tcp,
@@ -7,10 +8,15 @@ _methods = {
 }
 
 def get_method(method_name):
+    """Получение метода по имени"""
     return _methods.get(method_name.lower())
 
 def available_methods():
+    """Список доступных методов"""
     return list(_methods.keys())
 
 def register_method(name, method):
+    """Регистрация нового метода"""
+    if name.lower() in _methods:
+        raise ValueError(f"Метод {name} уже зарегистрирован")
     _methods[name.lower()] = method
